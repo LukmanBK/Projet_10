74513b3c740a90b6efd11a5621622ce458fbfdbb
LukmanBK <lukmanbakari@gmail.com>
2024-01-10 17:34:28 +0100

    correctifs 'imageSrc' et 'title' undefined et formatage dans le composant Logo

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,16 @@
-/**
- * 
- */
+import { getMonth } from "./index";
+
+
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+            const date = '2022-01-01T20:28:45.744Z';
+            expect(getMonth(new Date(date))).toBe('janvier');
         });
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            const date = '2022-07-08T20:28:45.744Z';
+            expect(getMonth(new Date(date))).toBe('juillet');
         });
     });
 })
-
